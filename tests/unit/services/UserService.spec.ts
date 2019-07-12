e1af5858a0bf778f36bbd791686764be6b258ff6
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2019-07-12 11:06:11 +0900

    test: axios-mock-adapterを使用してテストを書き換え

--- tests/unit/services/UserService.spec.ts
+++ tests/unit/services/UserService.spec.ts
@@ -1,17 +1,27 @@
 /* tslint:disable:no-unused-expression */
 
 import { expect } from 'chai';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import UserService from '@/services/UserService';
 import { User } from '@/services/types';
 
 describe('UserService', () => {
   let userService: UserService;
+  let mockAxios: MockAdapter;
 
   beforeEach(() => {
     userService = new UserService();
+    mockAxios = new MockAdapter(axios);
   });
 
   it('findById with 1', async () => {
+    mockAxios.onGet('/users/1').reply(200, {
+      id: 1,
+      name: 'QB',
+      age: 18,
+    });
+
     const user = await userService.findById(1);
     expect(user.id).to.equal(1);
   });
@@ -21,6 +31,11 @@ describe('UserService', () => {
       name: 'kaname madoka',
       age: 14,
     } as User;
+
+    mockAxios.onPost('/users').reply(201, {
+      id: 10,
+      ...data,
+    });
 
     const user = await userService.register(data);
     expect(user.id).to.not.be.undefined;
